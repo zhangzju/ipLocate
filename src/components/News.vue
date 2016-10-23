08f2ec7fe62df2d73a180046210c68e5f2af088d
zhangzju <zhangway19921221@163.com>
2016-10-23 17:47:02 +0800

    need backend

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <message></message>
+  <div v-for="item in news">
+    <message :title='item.category' :text='item.title' :date='item.date' :link='item.url'></message>
+  </div>
   </div>
 </template>
 <script>
@@ -17,18 +19,43 @@ export default {
       return {
         news:[
           {
-            title: "国际",
-            text:"外媒：印度GDP增速已超中国 医疗教育仍远远落后",
+            category: "国际",
+            title:"外媒：印度GDP增速已超中国 医疗教育仍远远落后",
             date:"2016-10-23 01:25",
-            link:"http:\/\/mini.eastday.com\/mobile\/161023012557545.html?qid=juheshuju"
+            url:"http:\/\/mini.eastday.com\/mobile\/161023012557545.html?qid=juheshuju"
           }
         ]
       }
     },
 
-    ready(){
-      var that = this;
-    }
+    // ready(){
+    //   this.$http.get('http://v.juhe.cn/toutiao/index',{
+    //     type: 'guoji',
+    //     key: '92fd55e2615b1ad201c8e9fad0764d82'
+    //   }).then((data)=>{
+    //     this.$set('news',data.result.data)
+    //   }).then((error)=>{
+    //       console.error("data not found!");
+    //   })
+    // },
+
+    ready() {
+      let topNews = [
+        {
+          category: "国际",
+          title:"外媒：印度GDP增速已超中国 医疗教育仍远远落后",
+          date:"2016-10-23 01:25",
+          url:"http:\/\/mini.eastday.com\/mobile\/161023012557545.html?qid=juheshuju"
+        },
+        {
+          category: "国际",
+          title:"外媒：印度GDP增速已超中国 医疗教育仍远远落后",
+          date:"2016-10-23 01:25",
+          url:"http:\/\/mini.eastday.com\/mobile\/161023012557545.html?qid=juheshuju"
+        }
+      ]
+      this.$set('news', topNews);
+    },
 
 }
 
